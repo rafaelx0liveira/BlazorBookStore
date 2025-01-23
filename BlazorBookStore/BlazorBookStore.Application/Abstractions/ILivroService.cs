8d2d05d1e55ef6e89dcb9b01b901658ab2fa1f51
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-01-23 15:32:25 -0300

    feat: edit and remove book

---
+++ BlazorBookStore/BlazorBookStore.Application/Abstractions/ILivroService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorBookStore.Application.Abstractions;
+
+public class ILivroService
+{
+    // - Encapsula lógicas de negócios mais complexas que vão além das
+    // operações simples de banco de dados e  orquestra chamadas
+    // a repositórios, aplicando a lógica de negócios e fornecendo uma
+    // interface mais específica do domínio.
+    // - Separar a lógica de negócios em serviços facilita a testabilidade
+    // - Permite fazer uma separação de responsabilidades entre repositórios e serviços
+}
---
+++ BlazorBookStore/BlazorBookStore.Application/Services/LivroService.cs
@@ -0,0 +1,8 @@
+﻿using Livraria.Application.Abstractions;
+
+namespace Livraria.Application.Services;
+
+public class LivroService : ILivroService
+{
+    //Implementação 
+}
